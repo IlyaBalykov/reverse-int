1c704fea465f6684d9f219fc6271536ab7a86cdf
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-07-02 20:02:51 +0300

    feat: make solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+    let x = n.toString();
+    for (let i = x.length - 1; i >= 0 ; i--) {
+        if (x[i] !== "-") {
+            result += +x[i];
+        }
+    }
+    result = Number(result);
+    return result
 }
